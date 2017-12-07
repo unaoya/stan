03c90258606f9ee17b7047f50a28dcbee42e0bc0
unaoya <umezakinaoya@gmail.com>
2017-12-07 23:22:53 +0900

    add ggplot

--- mixnorm/clustering.R
+++ mixnorm/clustering.R
@@ -33,14 +33,14 @@ rng2 <- function(N, mu, Sigma0, Sigma1, Sigma2){
 }
 
 #真の分布のパラメータ
-N <- 100
-mu1 <- c(0.5,0)
-mu2 <- c(0,0.5)
+N <- 30
+mu1 <- c(0.1,0)
+mu2 <- c(0,0.1)
 Sigma0 <- matrix(data = c(1,0,0,1), nrow = 2)
 Sigma1 <- matrix(data = c(2,0,0,0.1), nrow = 2)
 Sigma2 <- matrix(data = c(0.1,0,0,2), nrow = 2)
 
-iter <- 2
+iter <- 100
 results <- data.frame(waic1 = rep(0,iter),
                       wbic1 = rep(0,iter),
                       waic2 = rep(0,iter),
@@ -49,16 +49,79 @@ results <- data.frame(waic1 = rep(0,iter),
 for(i in 1:iter){
   d <- rng1(N, mu1, mu2, Sigma1, Sigma2)
   fit <- stan(file = 'model1.stan', data = d,
-              iter = 5000, warmup = 1000)
+              iter = 6000, warmup = 3000)
   log_lik <- extract(fit)$log_likelihood
   results$waic1[i] <- waic(log_lik)
   results$wbic1[i] <- wbic(log_lik)
   fit2 <- stan(file = 'model2.stan', data = d,
-               iter = 5000, warmup = 1000)
+               iter = 6000, warmup = 3000)
   log_lik <- extract(fit2)$log_likelihood
   results$waic2[i] <- waic(log_lik)
   results$wbic2[i] <- wbic(log_lik)
+  print(i)
 }
 results
 fit
 fit2
+stan_trace(fit2, pars = "Sigma1")
+stan_trace(fit2, pars = "Sigma2")
+
+N <- 100
+iter <- 30
+results100 <- data.frame(waic1 = rep(0,iter),
+                      wbic1 = rep(0,iter),
+                      waic2 = rep(0,iter),
+                      wbic2 = rep(0,iter))
+
+for(i in 1:iter){
+  d <- rng1(N, mu1, mu2, Sigma1, Sigma2)
+  fit <- stan(file = 'model1.stan', data = d,
+              iter = 6000, warmup = 3000)
+  log_lik <- extract(fit)$log_likelihood
+  results100$waic1[i] <- waic(log_lik)
+  results100$wbic1[i] <- wbic(log_lik)
+  fit2 <- stan(file = 'model2.stan', data = d,
+               iter = 6000, warmup = 3000)
+  log_lik <- extract(fit2)$log_likelihood
+  results100$waic2[i] <- waic(log_lik)
+  results100$wbic2[i] <- wbic(log_lik)
+  print(i)
+}
+
+
+select.waic <- c()
+for(i in 1:50){
+  select.waic[i] <- ifelse(results$waic1[i] < results$waic2[i], 1, 2)
+}
+
+select.wbic <- c()
+for(i in 1:50){
+  select.wbic[i] <- ifelse(results$wbic1[i] < results$wbic2[i], 1, 2)
+}
+
+
+library(ggplot2)
+library(reshape2)
+df100 <- read.csv(file = "result100.csv")
+df30 <- read.csv(file = "result30.csv")
+
+sum(df100$select.waic==1)
+sum(df100$select.wbic==1)
+sum(df30$select.waic==1)
+sum(df30$select.wbic==1)
+
+df.waic.30 <- data.frame(model1 = df30[,2], model2 = df30[,4], samples = as.factor(rep(30, 100)))
+df.waic.100 <- data.frame(model1 = df100[,2], model2 = df100[,4], samples = as.factor(rep(100, 50)))
+df.wbic.30 <- data.frame(model1 = df30[,3], model2 = df30[,5], samples = as.factor(rep(30, 100)))
+df.wbic.100 <- data.frame(model1 = df100[,3], model2 = df100[,5], samples = as.factor(rep(100, 50)))
+df.waic <- rbind(melt(df.waic.30), melt(df.waic.100))
+df.wbic <- rbind(melt(df.wbic.30), melt(df.wbic.100))
+df.wbic
+g.waic <- ggplot(df.waic, aes(x = samples, y = value))
+g.waic <- g.waic + geom_boxplot(aes(colour = variable))
+g.waic <- g.waic + ggtitle("WAIC")
+plot(g.waic)
+g.wbic <- ggplot(df.wbic, aes(x = samples, y = value))
+g.wbic <- g.wbic + geom_boxplot(aes(colour = variable))
+g.wbic <- g.wbic + ggtitle("WBIC")
+plot(g.wbic)
